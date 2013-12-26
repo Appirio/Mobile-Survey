1c78472495fc24eea4385d30e46368bb239cefe9
Mauricio Desiderio <mdesiderio@appirio.com>
2013-12-26 14:00:25 -0600

    testing 1000 accounts service

---
+++ app/com/appirio/diageo/db/manager/api14/AccountDBManager14.java
@@ -0,0 +1,15 @@
+package com.appirio.diageo.db.manager.api14;
+
+import com.appirio.diageo.db.DiageoServicesException;
+import com.appirio.diageo.db.manager.AccountDBManager;
+
+
+public class AccountDBManager14 extends AccountDBManager {
+
+	public AccountDBManager14() throws DiageoServicesException {
+		super();
+	}
+	
+	
+
+}
