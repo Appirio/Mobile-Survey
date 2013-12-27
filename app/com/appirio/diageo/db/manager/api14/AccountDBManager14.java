2fb2ddd22eb11987ce194454ffc71f54f33b8513
Mauricio Desiderio <mdesiderio@appirio.com>
2013-12-27 14:13:10 -0600

    new 1.4 service that allows specifying limit to number of accounts returned

--- app/com/appirio/diageo/db/manager/api14/AccountDBManager14.java
+++ app/com/appirio/diageo/db/manager/api14/AccountDBManager14.java
@@ -2,6 +2,10 @@ package com.appirio.diageo.db.manager.api14;
 
 import com.appirio.diageo.db.DiageoServicesException;
 import com.appirio.diageo.db.manager.AccountDBManager;
+import com.appirio.diageo.geolocation.HaversineCalculator;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 
 
 public class AccountDBManager14 extends AccountDBManager {
@@ -10,6 +14,22 @@ public class AccountDBManager14 extends AccountDBManager {
 		super();
 	}
 	
-	
-
+	public ArrayNode findAccounts(Double latitude, Double longitude, Double radius, long limit) throws DiageoServicesException {
+		double degrees = radius / 50;
+		
+		ArrayNode result = queryToJson("select latitude__c, longitude__c, tdlinx_outlet_city__c,  tdlinx_outlet_zip_code__c,  tdlinx_outlet_state__c,  tdlinx_outlet_addr__c,  billingstate,  billingcountry,  billingpostalcode, billingcity,  billingstreet,  sfid,  TDLinx_Outlet_Desc__c,  Name,  TDLinx_Outlet_State__c,  (|/( power(Latitude__c - " + latitude.toString() + ", 2) + power(Longitude__c - " + longitude.toString() + ", 2))) as distance from account where Latitude__c between " + Double.toString(latitude - degrees) + " and " + Double.toString(latitude + degrees) + " and Longitude__c between " + Double.toString(longitude - degrees) + " and " + Double.toString(longitude + degrees) + " order by distance limit " + Long.toString(limit) + ";");
+		
+		for(JsonNode item : result) {
+			ObjectNode obj = (ObjectNode) item;
+			
+			obj.put("distance", HaversineCalculator.calculateHaversine(obj.get("latitude__c").asDouble(),
+					   obj.get("longitude__c").asDouble(),
+					   latitude, longitude));
+			
+			obj.remove("latitude__c");
+			obj.remove("longitude__c"); 
+		}
+		
+		return result;
+	}
 }
--- app/controllers/AccountServices.java
+++ app/controllers/AccountServices.java
@@ -8,10 +8,11 @@ import play.mvc.With;
 
 import com.appirio.diageo.db.DiageoServicesException;
 import com.appirio.diageo.db.manager.AccountDBManager;
+import com.appirio.diageo.db.manager.api14.AccountDBManager14;
 
 public class AccountServices extends Controller {
 	
-	//@With(SecureAction.class)
+	@With(SecureAction.class)
     public static Result getAccounts(String latitude, String longitude, String radius) {
     	try {
     		AccountDBManager manager = new AccountDBManager();
@@ -19,6 +20,31 @@ public class AccountServices extends Controller {
     		
     		try {
     			result = manager.findAccounts(Double.parseDouble(latitude), Double.parseDouble(longitude), Double.parseDouble(radius));
+    		} finally {
+    			manager.close();
+    		}
+    		
+    		return ok(result);
+    		
+    	} catch (DiageoServicesException e) {
+    		e.printStackTrace();
+    		
+    		return internalServerError(ControllerUtils.messageToJson(e.getMessage()));
+    	} catch (Exception e) {
+    		e.printStackTrace();
+    		
+    		return internalServerError(ControllerUtils.messageToJson("An unexpected error occurred!"));
+    	}
+    }
+	
+	@With(SecureAction.class)
+    public static Result getAccountsLimited(String latitude, String longitude, String radius, String limit) {
+    	try {
+    		AccountDBManager14 manager = new AccountDBManager14();
+    		ArrayNode result = null;
+    		
+    		try {
+    			result = manager.findAccounts(Double.parseDouble(latitude), Double.parseDouble(longitude), Double.parseDouble(radius), Long.parseLong(limit));
     		} finally {
     			manager.close();
     		}
