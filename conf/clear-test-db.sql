ae1fb9da4113bf32a9271c110bd29c1fa63d1b56
Mauricio Desiderio <mdesiderio@appirio.com>
2014-05-02 15:05:36 -0500

    adding test of create survey

--- conf/clear-test-db.sql
+++ conf/clear-test-db.sql
@@ -3,3 +3,5 @@ delete from dd_my_accounts__c;
 delete from dms_question__c;
 delete from dms_survey__c;
 delete from _trigger_log;
+delete from dd_survey_submission__c;
+delete from dms_survey_result__c;
