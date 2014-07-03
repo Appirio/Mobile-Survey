a5a28f55e628ede4e21a1f4f2496331dfb6f2a02
Mauricio Desiderio <mdesiderio@appirio.com>
2014-07-03 08:25:47 -0500

    working on goal scoring

--- conf/query-goals-for-processing.sql
+++ conf/query-goals-for-processing.sql
@@ -13,6 +13,7 @@ FROM
 	INNER JOIN dd_assigned_goal__c ag ON ag.contact__c = ''{1}'' AND ag.dd_survey_question__c = q.sfid
 WHERE
 	(q.is_goal__c is not null AND q.is_goal__c = true) AND
-    q.sfid IN (SELECT question__c from dms_survey_result__c where dd_survey_submission__c__external_id__c = ''{0}'')
+    q.sfid IN (SELECT question__c from dms_survey_result__c where dd_survey_submission__c__external_id__c = ''{0}'') AND
+    r.contact__c = ''{1}''
 ORDER BY
 	q.sfid;
