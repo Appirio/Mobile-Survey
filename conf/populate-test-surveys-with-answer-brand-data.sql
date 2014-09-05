6ca4c100b5c548b457a4c2ba8c903146c856bd49
AjayHada <ahada@appirio.com>
2014-09-05 17:11:05 +0530

    test data modified

--- conf/populate-test-surveys-with-answer-brand-data.sql
+++ conf/populate-test-surveys-with-answer-brand-data.sql
@@ -4,10 +4,57 @@ INSERT INTO contact (
 	'005J0000001q3z6IAA'
 );
 
-INSERT INTO brand__c VALUES ('3', '2014-07-09 04:03:36', 40, 'USA', 'Cape Floral Chenin Blanc 0.75L 12x01 STANDARD', 'Configuration & Container', false, NULL, '2014-07-09 04:01:11', 'L6', 'a07J0000008L7yFIAS', NULL);
-INSERT INTO brand__c VALUES ('3', '2014-07-09 04:03:29', 41, 'USA', 'Cape Floral Chenin Blanc 0.75L', 'Volume', false, NULL, '2014-07-09 04:00:31', 'L6', 'a07J0000008L7yAIAS', NULL);
-INSERT INTO brand__c VALUES ('3', '2014-07-01 11:18:26', 38, 'USA', 'Captain Morgan White Rum', 'Individual Variant', false, NULL, '2014-06-25 15:11:58', 'L5', 'a07J0000008IBTmIAO', NULL);
 
-INSERT INTO dms_survey__c VALUES (NULL, NULL, NULL, 'a2iJ0000000AAlYIAW', false, NULL, false, NULL, NULL, NULL, NULL, 3, NULL, '005J0000001q3z6IAA', 197, '2014-07-25 16:54:00', 'Example Survey 25July', NULL, NULL, '2014-07-25 16:54:00', '005J0000001q3z6IAA', '005J0000001q3z6IAA', true, 'Non Product', NULL, false, NULL, 'a2gJ0000000DAx3IAG', '2014-07-25 16:43:54', 3, true, NULL, NULL);
+INSERT INTO brand__c (Id, Name,Country__c, Parent_Brand__c,Brand_Priority__c,Brand_Type__c, Level__c,IsDeleted,CreatedDate,LastModifiedDate) VALUES ('91', 'Cape Floral Chenin Blanc 0.75L 12x01 STANDARD','USA',null, '3','Configuration & Container' ,'L6', false,'2014-07-09 04:03:36','2014-07-09 04:01:11' );
+INSERT INTO brand__c (Id, Name,Country__c, Parent_Brand__c,Brand_Priority__c,Brand_Type__c, Level__c,IsDeleted,CreatedDate,LastModifiedDate) VALUES ('92', 'Cape Floral Chenin Blanc 0.75L','USA',null, '3','Volume' ,'L6', false,'2014-07-09 04:03:36','2014-07-09 04:01:11' );
+INSERT INTO brand__c (Id, Name,Country__c, Parent_Brand__c,Brand_Priority__c,Brand_Type__c, Level__c,IsDeleted,CreatedDate,LastModifiedDate) VALUES ('93', 'Captain Morgan White Rum','USA',null, '3','Individual Variant' ,'L5', false,'2014-07-09 04:03:36','2014-07-09 04:01:11' );
+
+
+
+INSERT INTO dms_survey__c (
+	name, 
+	sfid,
+	total_possible_score__c,
+	survey_type__c,
+	category__c
+) VALUES (
+	'Survey 1',
+	'a2iJ0000000AAlYIAW',
+	'10',
+	'Product',
+	'Value1'
+);
+
+INSERT INTO dms_question__c (
+	sfid, 
+	question_text__c, 
+	parent_question__c, 
+	name, 
+	question_type__c, 
+	dms_survey__c
+) VALUES (
+	'a2gJ0000000DAx3IAG',
+	'the question text',
+	null,
+	'question 2',
+	'Count',
+	'a2iJ0000000AAlYIAW'
+);
+
+INSERT INTO dms_question__c (
+	sfid, 
+	question_text__c, 
+	parent_question__c, 
+	name, 
+	question_type__c, 
+	dms_survey__c
+) VALUES (
+	'a2gJ0000000DAx8IAG',
+	'the question text',
+	null,
+	'question 2',
+	'Count',
+	'a2iJ0000000AAlYIAW'
+);
 
 
