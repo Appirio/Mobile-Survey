cf47235678184005025745c4db0ed5e8f015415a
Parimal Rane <prane@appirio.com>
2015-07-16 12:07:50 -0400

    getAccounts17 removed

--- app/controllers/AccountServices.java
+++ app/controllers/AccountServices.java
@@ -62,32 +62,4 @@ public class AccountServices extends Controller {
     		return internalServerError(ControllerUtils.messageToJson("An unexpected error occurred!"));
     	}
     }
-	
-	@With(SecureAction.class)
-    public static Result getAccounts17(String latitude, String longitude, String radius) {
-    	try {
-    		String userId = request().getHeader("uid");
-    		
-    		AccountDBManager17 manager = new AccountDBManager17(userId);
-    		JsonNode result = null;
-    		
-    		try {
-    			result = manager.findAccounts(Double.parseDouble(latitude), Double.parseDouble(longitude), Double.parseDouble(radius));
-    		} finally {
-    			manager.close();
-    		}
-    		
-    		return ok(result);
-    		
-    	} catch (DiageoServicesException e) {
-    		e.printStackTrace();
-    		
-    		return internalServerError(ControllerUtils.messageToJson(e.getMessage()));
-    	} catch (Exception e) {
-    		e.printStackTrace();
-    		
-    		return internalServerError(ControllerUtils.messageToJson("An unexpected error occurred!"));
-    	}
-    }
-	
 }
