484879c377d329b185a837d2ef9e72294639d233
ericab <ericabyrd4011@gmail.com>
2015-09-04 14:32:34 -0400

    updating class/package names

--- app/com/appirio/diageo/db/manager/api15/AccountDBManager15.java
+++ app/com/appirio/diageo/db/manager/api15/AccountDBManager15.java
@@ -1,8 +1,8 @@
-package com.appirio.diageo.db.manager.api15;
+package com.appirio.mobilesurvey.db.manager.api15;
 
 import java.text.MessageFormat;
 
-import com.appirio.diageo.db.DiageoServicesException;
+import com.appirio.diageo.db.MSServicesException;
 import com.appirio.diageo.db.manager.api14.AccountDBManager14;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
@@ -10,11 +10,11 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 
 public class AccountDBManager15 extends AccountDBManager14 {
 
-	public AccountDBManager15() throws DiageoServicesException {
+	public AccountDBManager15() throws MSServicesException {
 		super();
 	}
 	
-	public JsonNode findAccounts(Double latitude, Double longitude, Double radius, long limit) throws DiageoServicesException {
+	public JsonNode findAccounts(Double latitude, Double longitude, Double radius, long limit) throws MSServicesException {
 		double degrees = radius / 50;
 		
 		ArrayNode result = queryToJson(MessageFormat.format(getSQLStatement("accounts-query-15"), latitude, longitude, latitude - degrees, latitude + degrees, longitude - degrees, longitude + degrees, limit));
