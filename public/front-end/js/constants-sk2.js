4845d07fc29620ca0845f6f058b4e996f240b13d
ericab <ericabyrd4011@gmail.com>
2015-09-04 15:05:39 -0400

    updating class/package names

--- public/front-end/js/constants-sk2.js
+++ public/front-end/js/constants-sk2.js
@@ -1,16 +1,11 @@
-// This file is used by the automated build process for android to build the sk2 version of the app
-/* Constants.js for Diageo Mobile Survey App*/
+/* Constants.js for Mobile Survey App*/
 
 //var BUILD_NUMBER = '0.0.1';
 var VERSION_NUMBER = '2.0.2';
 var SERVICE_BUILD_NUMBER = '/2.0';
 var ACTIVE_ENV = "SK2";
 
-//var BASE_PATH = 'http://localhost:9000';
-//var BASE_PATH = 'http://diageo-survey-services-dd3qa.herokuapp.com';  //'https://shielded-river-6617.herokuapp.com';	//This points to dd3qa org
-//var BASE_PATH = 'http://diageo-survey-services-sfadev.herokuapp.com'; //this points to sfadev
-var BASE_PATH = 'https://heroku-cx-survey-dev.herokuapp.com';	//This points to sk2 org
-//var BASE_PATH = 'https://diageo-survey-services.herokuapp.com'; //This points to production org
+var BASE_PATH = '';	
 
 var AUTHORIZE_PATH = BASE_PATH + SERVICE_BUILD_NUMBER + '/contact/authorize';
 var ACCOUNT_GEOLOCATION_PATH = BASE_PATH + SERVICE_BUILD_NUMBER + '/account/:latitudeCoord/:longitudeCoord/:miles';
@@ -21,7 +16,7 @@ var SURVEY_SUBMIT_PATH = BASE_PATH + SERVICE_BUILD_NUMBER + '/survey';
 var GOAL_DASHBOARD_PATH = BASE_PATH + SERVICE_BUILD_NUMBER + '/goal/achievements/:startdate/:enddate/:includeDetails';
 var CONTACT_INFO_PATH = BASE_PATH + SERVICE_BUILD_NUMBER + '/contact';
 
-var T_AND_C_PATH = 'http://diageodirect.force.com/DMS_Terms_Page';
+var T_AND_C_PATH = 'http://appirio.com';
 
 //variables used for localStorage
 var CACHED_SURVEY_DATA = 'SurveyData';
