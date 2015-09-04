d54ca8319f8989da6d796e374a67e069fece972a
ericab <ericabyrd4011@gmail.com>
2015-09-04 15:06:31 -0400

    updating class/package names

--- public/front-end/js/constants.js
+++ public/front-end/js/constants.js
@@ -5,11 +5,6 @@ var VERSION_NUMBER = '2.0.2';
 var SERVICE_BUILD_NUMBER = '/2.0';
 var ACTIVE_ENV = "PRODUCTION";
 
-//var BASE_PATH = 'http://localhost:9000';
-//var BASE_PATH = 'http://diageo-survey-services-dd3qa.herokuapp.com';  //'https://shielded-river-6617.herokuapp.com';	//This points to dd3qa org
-//var BASE_PATH = 'http://diageo-survey-services-sfadev.herokuapp.com'; //this points to sfadev
-//var BASE_PATH = 'https://diageo-survey-services-sk2.herokuapp.com';	//This points to sk2 org
-//var BASE_PATH = 'http://heroku-cx-survey-dev.herokuapp.com'; //This points to production org
 
 var BASE_PATH =""
 
@@ -22,7 +17,7 @@ var SURVEY_SUBMIT_PATH = BASE_PATH + SERVICE_BUILD_NUMBER + '/survey';
 var GOAL_DASHBOARD_PATH = BASE_PATH + SERVICE_BUILD_NUMBER + '/goal/achievements/:startdate/:enddate/:includeDetails';
 var CONTACT_INFO_PATH = BASE_PATH + SERVICE_BUILD_NUMBER + '/contact';
 
-var T_AND_C_PATH = 'http://diageodirect.force.com/DMS_Terms_Page';
+var T_AND_C_PATH = 'http://appirio.com';
 
 //variables used for localStorage
 var CACHED_SURVEY_DATA = 'SurveyData';
